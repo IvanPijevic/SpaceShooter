0ab742c8e2fc906d440df6f119ed80b4339b15e6
Asfodel <ivan.pijevic@gmail.com>
2025-02-17 21:47:26 +0100

    sdl-processInput

--- SpaceShooter/System.cpp
+++ SpaceShooter/System.cpp
@@ -14,28 +14,25 @@ void System::initGame()
 
 void System::processInput()
 {
-	HANDLE handleInput = GetStdHandle(STD_INPUT_HANDLE);
-	INPUT_RECORD inputRecord;
-	DWORD eventsRead;
-	bool evnt = true;
-
-	while (evnt)
+	SDL_Event evnt;
+	while (SDL_PollEvent(&evnt))
 	{
-		ReadConsoleInput(handleInput, &inputRecord, 1, &eventsRead);
-
-		switch (inputRecord.EventType)
+		switch (evnt.type)
 		{
-		case KEY_EVENT:
-			m_input.pressKey(inputRecord.Event.KeyEvent.bKeyDown);
-			std::cout << "KEYKEY" << std::endl;
-			break;
+			case SDL_EVENT_QUIT:
+				m_gameState = GameState::QUIT;
+				break;
+
+			case SDL_EVENT_KEY_DOWN:
+				m_input.pressKey(evnt.key.key);
+				break;
+
+			case SDL_EVENT_KEY_UP:
+				m_input.releaseKey(evnt.key.key);
+				break;
 		}
 	}
 
-	//if (m_input.isKeyDown())
-	//{
-	//	std::cout << "W key" << std::endl;
-	//}
 }
 
 void System::gameLoop()
